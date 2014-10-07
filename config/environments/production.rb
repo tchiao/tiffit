4e50869412bd4a882b3d48574dd83bd1f6027392
tchiao <tiffanya.chiao@gmail.com>
2014-10-07 16:40:37 -0700

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # http://tiff-it.herokuapp.com
+  config.action_mailer.default_url_options = {host: 'tiff-it.herokuapp.com'}
 end
