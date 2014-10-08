efc15b368f0064e8003ef590b334ea13117f87d8
tchiao <tiffanya.chiao@gmail.com>
2014-10-07 17:54:45 -0700

    Refactored controllers and views.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -10,7 +10,7 @@ class TopicsController < ApplicationController
   end
 
   def create
-    @topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
+    @topic = Topic.new(topic_params)
     authorize @topic
     if @topic.save
       flash[:notice] = "Topic was created."
@@ -35,12 +35,17 @@ class TopicsController < ApplicationController
   def update
     @topic = Topic.find(params[:id])
     authorize @topic
-    if @topic.update_attributes(params.require(:topic).permit(:name, :description, :public))
+    if @topic.update_attributes(topic_params)
       flash[:notice] = "Topic was updated."
       redirect_to @topic
     else
       flash[:error] = "There was an error updating the topic."
       render :edit
     end
+  end
+
+  private
+  def topic_params
+    params.require(:topic).permit(:name, :description, :public)
   end
 end
