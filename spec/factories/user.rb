2485eb07c56b8369c4105980ccedc6f6862d1eb3
tchiao <tiffanya.chiao@gmail.com>
2014-10-23 11:37:33 -0700

    Added user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,5 +5,12 @@ FactoryGirl.define do
     password "password"
     password_confirmation "password"
     confirmed_at Time.now
+
+    factory :user_with_post_and_comment do
+      after(:build) do |user|
+        post = create(:post, user: user)
+        create(:comment, post: post, user: user)
+      end
+    end
   end
 end
