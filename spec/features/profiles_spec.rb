a8435a063c9ad345c1762b1f48f490f5880cb063
tchiao <tiffanya.chiao@gmail.com>
2014-10-22 17:20:42 -0700

    Added user profile test

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 describe "Visiting profiles" do
   include TestFactories
+  include Warden::Test::Helpers
+  Warden.test_mode!
 
   before do
     @user = authenticated_user
     @post = associated_post(user: @user)
-    @comment = Comment.new(user: @user, body: "Here is a comment")
+    @comment = Comment.new(user: @user, body: "Here is a comment", post: @post)
     allow(@comment).to receive(:send_favorite_emails)
     @comment.save
   end
@@ -18,7 +20,20 @@ describe "Visiting profiles" do
       expect(page).to have_content(@user.name)
       expect(page).to have_content(@post.title)
       expect(page).to have_content(@comment.body)
+    end
+  end
 
+  describe "signed in" do
+    before do
+      login_as(@user, :scope => :user)
+    end
+
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@post.title)
+      expect(page).to have_content(@comment.body)
     end
   end
 end
